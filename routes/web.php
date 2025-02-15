868a27bf4885fc542ae466fe299191d37fcd7850
Florian Rodriguez <florian@negative-network.com>
2025-02-15 15:47:42 +0100

    Cours du 12/02

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ use App\Http\Controllers\PagesController;
 Route::get('/', [PagesController::class, 'home']);
 Route::get('/about', [PagesController::class, 'about']);
 Route::get('/contact', [PagesController::class, 'contact']);
+
+use App\Http\Controllers\ProjectController;
+Route::get('/project', [ProjectController::class, 'index']);
